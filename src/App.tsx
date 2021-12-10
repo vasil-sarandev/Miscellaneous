3c24fe0a80bd334bef703366fb9ad7023419ddd4
vsrndv <72616740+vsrndv@users.noreply.github.com>
2021-12-10 11:16:50 +0200

    delete boilerplate from create react app, add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import logo from './logo.svg'
-import './App.css'
-
 const App = () => (
   <div className='App'>
     <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
