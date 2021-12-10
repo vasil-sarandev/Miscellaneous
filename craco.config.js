2b912a554044a479238fe0beaaee48157b18d1e0
vsrndv <72616740+vsrndv@users.noreply.github.com>
2021-12-10 12:55:51 +0200

    add craco and working aliases

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const CracoAlias = require('craco-alias')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'options',
+        aliases: {
+          '@shared': './src/shared',
+          '@pages': './src/pages'
+        }
+      }
+    }
+  ]
+}
